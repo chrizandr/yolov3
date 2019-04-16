e47efd99a0cf041027af02eac3c7290616e846ee
Chris Andrew <chrisandrew119@gmail.com>
2019-04-16 13:44:56 +0530

    Eval.

--- evaluate.py
+++ evaluate.py
@@ -10,11 +10,36 @@ def read_output_file(filename):
         for line in f:
             d = line.split()
             d = [int(float(x)) for x in d[0:4]]
+            annotations.append(d)
+    return np.array(annotations)
 
 
-def evaluate_person(output_dir, data_dir):
+def evaluate_person(output_dir, data_dir, annotation_file):
     files = os.listdir(data_dir)
     annot_files = os.listdir(output_dir)
+    output_imgs = [x.split(".txt")[0] for x in annot_files]
+    annotations = get_scaled_annotations_person(annotation_file)
+    correct = 0
+    total = 0
+    for f in files:
+        annotation = annotations[f]
+        total = total + annotation.shape[0]
+        if f in output_imgs:
+            output = read_output_file(os.path.join(output_dir, f + ".txt"))
+            count = match_annotations(output, annotation)
+            correct += count
+    return float(correct)/total * 100
+
+
+def match_annotations(output, annotation):
+    count = 0
+    for o in output:
+        dist = np.sum((annotation - o) ** 2, axis=1)
+        closest_box = annotation[p.argmin(dist), :]
+        iou = bb_intersection_over_union(closest_box, o)
+        if iou > 0.8:
+            count += 1
+    return count
 
 
 def bb_intersection_over_union(boxA, boxB):
