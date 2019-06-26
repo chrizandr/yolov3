9334b8f2fcfc1b39addd87d13ae585b7dbe03648
Chris Andrew <chrisandrew119@gmail.com>
2019-06-26 23:29:17 +0530

    fd

--- data/get_coco_dataset.sh
+++ data/get_coco_dataset.sh
@@ -6,12 +6,12 @@ git clone https://github.com/pdollar/coco && cd coco
 
 # Download Images
 mkdir images && cd images
-wget -c https://pjreddie.com/media/files/train2014.zip
-wget -c https://pjreddie.com/media/files/val2014.zip
+#wget -c https://pjreddie.com/media/files/train2014.zip
+#wget -c https://pjreddie.com/media/files/val2014.zip
 
 # Unzip
-unzip -q train2014.zip
-unzip -q val2014.zip
+#unzip -q train2014.zip
+#unzip -q val2014.zip
 
 # (optional) Delete zip files
 rm -rf *.zip
