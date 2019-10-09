8b5afeeea25e35324d6646bada0f8aa1102da5ed
Chris <chrisandrew119@gmail.com>
2019-10-09 16:30:33 +0530

    Training

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 # python3 train.py --img-size 1024 --transfer --batch-size 4
-python train.py --img-size 416 --transfer --batch-size 4 --freeze --save_per_epoch 10 --multi-scale
+# python train.py --img-size 416 --transfer --batch-size 4 --freeze --save_per_epoch 10 --multi-scale
+python train.py --img-size 416 --transfer --batch-size 4 --freeze --save_per_epoch 10 --multi-scale --cfg cfg/soccer.cfg  --data-cfg data/soccer.data
 # python3 train.py --img-size 1024 --transfer --batch-size 4 --save_per_epoch 10 --data-cfg data/coco2.data
